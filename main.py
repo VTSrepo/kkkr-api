b9a646f59ad6d8879b312050db76fb93524292bf
VTSRepo <vibhavatechnologyservices@gmail.com>
2024-01-13 16:59:17 +1030

    added UI build

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 from read_org_master import get_org_details
 from get_profiles import get_profiles
 from flask import jsonify, make_response
 from flask_cors import CORS
-app = Flask(__name__)
+app = Flask(__name__, static_folder='./web', static_url_path='/')
 CORS(app)
-@app.route("/")
+@app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/")
+def index():
+    return app.send_static_file('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
